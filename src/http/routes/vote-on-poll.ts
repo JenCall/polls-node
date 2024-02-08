d42dea922c993d0b485f58d154b572884732a215
Jen <jenifferprc@gmail.com>
2024-02-08 21:43:10 +0100

    Add cookies and vote-on-poll sessionId

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,11 +1,12 @@
 import { z } from "zod";
+import { randomUUID } from "node:crypto";
 import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (request, reply) => {
     const voteOnPollBody = z.object({
-      pollOptionId: z.string(),
+      pollOptionId: z.string().uuid(),
     });
 
     const voteOnPollParams = z.object({
@@ -15,6 +16,19 @@ export async function voteOnPoll(app: FastifyInstance) {
     const { pollId } = voteOnPollParams.parse(request.params);
     const { pollOptionId } = voteOnPollBody.parse(request.body);
 
-    return reply.status(201).send();
+    let sessionId = request.cookies.sessionId;
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.setCookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 60 * 60 * 24 * 30,
+        signed: true,
+        httpOnly: true,
+      });
+    }
+
+    return reply.status(201).send({ sessionId });
   });
 }
